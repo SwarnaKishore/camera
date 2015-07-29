9c91504ce4e472d2082103d7bdd73a9e6716037c
Swarna Kishore <swarnakishoree@gmail.com>
2015-07-29 15:29:24 -0500

    basic filter logic

--- camera.js
+++ camera.js
@@ -1,42 +1,39 @@
-Photos = new Meteor.Collection("photos");
+
 if (Meteor.isClient) {
 
   Template.takePhoto.events({
     'click .capture': function(){
       MeteorCamera.getPicture(function (error, data) {
-        if (! error) {
-          onSuccess(data);
-        }
+        Session.set('photo', data);
       });
     },
-    'click #blur': function(){
-      //console.log('button click method');
-      //  Session.set('filters', 'blur');
-      //  console.log(Session.get('filters'));
-    }
-  });
-
-
-  var onSuccess = function (imageData) {
-    Photos.insert({
-      image: imageData,
-      createdAt: new Date(),
-    });
-
-  };
-
-
-    Template.list.helpers({
-    photos: function () {
-      return Photos.find({}, {sort: {"createdAt": -1}});
+      'click #blur': function(){
+      $( "img" ).removeClass( "grayscale invert brightness" ).addClass( "blur" );
     },
-    filters: function(){
-      //return Session.get('filters');
-     // console.log(Session.get('filters'));
+
+    'click #grayscale': function(){
+       $( "img" ).removeClass( "blur invert brightness" ).addClass( "grayscale" );
+      
+    },
+
+    'click #invert': function(){
+      $( "img" ).removeClass( "blur grayscale brightness" ).addClass( "invert" );
+  
+    },
+
+    'click #brightness': function(){
+      $( "img" ).removeClass( "grayscale invert blur" ).addClass( "brightness" );
+     
     }
-
-
   });
+
+
+Template.takePhoto.helpers({
+  'photo': function(){
+    return Session.get('photo');
+  }
+});
+
   
 }//isClient
 
